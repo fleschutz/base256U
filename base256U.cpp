d98e8ceb60b78fb143c5d547034f83bb43710a08
Markus Fleschutz <markus.fleschutz@gmail.com>
2025-03-24 13:05:26 +0100

    Update base256U.cpp

--- base256U.cpp
+++ base256U.cpp
@@ -26,6 +26,7 @@ Unicode alphabet[256] =
 354,355,356,357,358,359,360,361,362,363,364,365,366,367,368,369,
 370,371,372,373,374,375,376,377,378,379,380,381,382,383,384,385,
 386,387
+// skipped × (=215) and ÷ (=247) - both are math operators, not letters
 };
 
 string encodeBase256U(const uint8_t *dataPtr, size_t dataSize)
